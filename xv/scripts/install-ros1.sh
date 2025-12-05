f3517443157f6a0a72577bcc632fb1c890b4ddd5
jinming <411893818@qq.com>
2025-12-05 13:54:11 +0800

    优化ros1安装脚本

--- xv/scripts/install-ros1.sh
+++ xv/scripts/install-ros1.sh
@@ -4,7 +4,7 @@ chmod 777 *
 
 echo "install environment"
 apt-get update
-apt-get install -y tree g++ cmake cmake-curses-gui pkg-config autoconf libtool libudev-dev libjpeg-dev zlib1g-dev libopencv-dev rapidjson-dev libeigen3-dev libboost-thread-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-date-time-dev liboctomap-dev doxygen libcxsparse3 libsuitesparse-dev
+apt-get install -y tree g++ cmake cmake-curses-gui pkg-config autoconf libtool libudev-dev libjpeg-dev zlib1g-dev libopencv-dev rapidjson-dev libeigen3-dev libboost-thread-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-date-time-dev liboctomap-dev doxygen libcxsparse3 libsuitesparse-dev ros-noetic-ddynamic-reconfigure
 apt-get install --fix-broken -y
 sleep 1
 
@@ -25,7 +25,14 @@ cp -r /usr/share/ros-wrapper/xv_sdk ~/catkin_ws/src/
 echo "compile ros1 wrapper"
 cd ~/catkin_ws/
 rosdep install --from-paths src --ignore-src -r -y
+
 source /opt/ros/noetic/setup.bash
+source ${HOME}/catkin_ws/devel/setup.bash
+
 catkin_make -DXVSDK_INCLUDE_DIRS="/usr/include/xvsdk" -DXVSDK_LIBRARIES="/usr/lib/libxvsdk.so"
 chmod 777 -R ~/catkin_ws/
-source ~/catkin_ws/devel/setup.bash
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source ${HOME}/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
